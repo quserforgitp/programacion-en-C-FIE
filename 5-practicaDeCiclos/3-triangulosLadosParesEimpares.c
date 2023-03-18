ff55c6b5e9283c233bd197a1f7d82b5dd47464f3
qAutHorp <adealumnonegligas@gmail.com>
2023-03-18 09:37:20 -0600

    informa del numero de combinaciones posibles,  despliega todas las combinaciones y determina si es par o impar. Falta que filtre las que son iguales

--- 5-practicaDeCiclos/3-triangulosLadosParesEimpares.c
+++ 5-practicaDeCiclos/3-triangulosLadosParesEimpares.c
@@ -3,7 +3,8 @@
 int main(int argc, char const *argv[])
 {
 
-  int m,n,numeroTriangulosDistintos,ladoA,ladoB,ladoC;
+  int m,n,numeroTriangulosDistintos,ladoA,ladoB,ladoC,
+      p,q; // p -> impar, q -> par
   float combinacionesPosibles;
 
   // recibiendo intervalo cerrado de 'm' a 'n'
@@ -13,14 +14,29 @@ int main(int argc, char const *argv[])
   ladoA = ladoB = ladoC = m; // -> iniciamos en (por ejemplo) 3 3 3
   combinacionesPosibles = pow(((n - m) + 1), 3.0);
 
-  printf("numero de combinaciones posibles = %.2f", combinacionesPosibles);
+  printf("numero de combinaciones posibles = %.0f", combinacionesPosibles);
   printf("\n");//separar lineas
 
   // combianciones posibles de intervalo
   for (int i = 0; i < combinacionesPosibles; i++)
   {
+    printf("Combinacion numero: %d\n", i+1);
+    
+    //determinando par e impar (se observa el comportamiento "uno si, uno no") se puede simplificar?
+    if ((ladoC % 2) == 0)
+    {
+      printf("numero par: ");
+      q++;
+    } else {
+      printf("numero impar: ");
+      p++;
+    }
+    
+    //pintando en consola el numero compuesto de 3 cifras
     printf("%d %d %d\n", ladoA, ladoB, ladoC);
     
+    // determinando cual de los numeros tiene que variar para seguir con la sucesion
+
     if ((ladoC == n) && (ladoB == n))
     {
       ladoA++;
@@ -34,5 +50,7 @@ int main(int argc, char const *argv[])
       ladoC++;
     }
   }
+  printf("\n\ncantidad de numeros pares:\t%d\n",q);
+  printf("cantidad de numeros impares:\t%d\n",p);
   return 0;
 }
