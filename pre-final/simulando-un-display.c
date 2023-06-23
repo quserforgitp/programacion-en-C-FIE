aef6533efba8496008d15a1e2a1590631d868b67
qAutHorp <adealumnonegligas@gmail.com>
2023-06-22 18:11:37 -0600

    meterElSeis() esta lista para su uso

--- pre-final/simulando-un-display.c
+++ pre-final/simulando-un-display.c
@@ -107,6 +107,32 @@ meterElCinco ( char matrizCuadrada[FILAS][COLUMNAS],
 }
 
 void
+meterElSeis ( char matrizCuadrada[FILAS][COLUMNAS],
+            int *espOcupados, char caracter )
+{
+  for (size_t i = 0; i < FILAS; i++)
+    for (size_t j = (*espOcupados); j < (*espOcupados) + 4; j++)
+    {
+      if (i%2 == 0) matrizCuadrada[i][j] = caracter; // 1,3,5 filas
+      else // 2,4 filas
+      {
+        if (i == 1) // fila 2
+        {
+          if (j == (*espOcupados) ) matrizCuadrada[i][j] = caracter;// 1 columnas
+            else matrizCuadrada[i][j] = ' '; // 2,3,4 columnas  
+        } 
+        else // fila 4
+          {
+            if (j == (*espOcupados) || j == (*espOcupados) + 2) matrizCuadrada[i][j] = caracter;// 1,3 columnas
+            else matrizCuadrada[i][j] = ' '; // 2,4 columnas
+          }
+      }
+      if ( j == (*espOcupados) + 3 ) matrizCuadrada[i][j] = ' '; // siempre en la 4 columna
+    }
+    (*espOcupados) += 4;
+}
+
+void
 meterElSiete( char matrizCuadrada[FILAS][COLUMNAS],
             int *espOcupados, char caracter )
 {
@@ -212,11 +238,11 @@ main ()
   meterElCero ( matrizCuadrada, &espOcupados, '#' );
   meterElUno ( matrizCuadrada, &espOcupados, '#' );
   meterElDos ( matrizCuadrada, &espOcupados, '#' );
-  meterElTres ( matrizCuadrada, &espOcupados, '3' );
-  meterElCinco ( matrizCuadrada, &espOcupados, '5');
+  meterElTres ( matrizCuadrada, &espOcupados, '#' );
+  meterElCinco ( matrizCuadrada, &espOcupados, '#' );
+  meterElSeis ( matrizCuadrada, &espOcupados, '#' );
   meterElSiete ( matrizCuadrada, &espOcupados, '#' );
   meterElOcho ( matrizCuadrada, &espOcupados, '#' );
-  meterElNueve ( matrizCuadrada, &espOcupados, '#' );
   meterElNueve ( matrizCuadrada, &espOcupados, '#' );
 
   imprimirMatriz( matrizCuadrada,espOcupados );
